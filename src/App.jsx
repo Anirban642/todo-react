5cf0d327c6f832073f462740fc51990152789290
Anirban Das <anirbandas64237@gmail.com>
2024-10-18 19:31:40 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ToDo from './Components/ToDo'
+
+const App = () => {
+  return (
+    <div className='grid py-4 min-h-screen bg-stone-900'>
+      <ToDo/>
+    </div>
+  )
+}
+
+export default App
